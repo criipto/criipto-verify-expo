4204c585a61261d07a3e9597b8506d8e88981c21
Mick Hansen <doomschild@gmail.com>
2023-07-17 08:55:33 +0200

    Swedish BankID (#1)
    * sebankid prep work
    
    * add basic transaction handling
    
    * code-exchange for Swedish BankID

---
+++ src/hooks/useAppState.ts
@@ -0,0 +1,29 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { AppState, AppStateStatus } from 'react-native';
+
+export default function useAppState(onForeground? : () => void, deps: React.DependencyList = []) {
+  const appState = useRef(AppState.currentState);
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', _handleAppStateChange);
+
+    return () => {
+    subscription.remove();
+    };
+  }, deps);
+
+  const _handleAppStateChange = (nextAppState : AppStateStatus) => {
+    if (
+      appState.current.match(/inactive|background/) &&
+      nextAppState === 'active'
+    ) {
+      if (onForeground) onForeground();
+    }
+
+    appState.current = nextAppState;
+    setAppStateVisible(appState.current);
+  };
+
+  return appStateVisible;
+}
--- src/provider.tsx
+++ src/provider.tsx
@@ -1,15 +1,18 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import {Platform} from 'react-native';
 import TextEncoding from 'text-encoding';
 import * as crypto from 'expo-crypto';
 import {decode, encode} from 'base-64';
 import jwtDecode from 'jwt-decode';
 import * as WebBrowser from 'expo-web-browser';
+import * as Linking from 'expo-linking';
 import 'react-native-url-polyfill/auto';
 import { buildAuthorizeURL, codeExchange, generatePlatformPKCE, OpenIDConfigurationManager } from '@criipto/oidc';
 
 import CriiptoVerifyContext, { CriiptoVerifyContextInterface, OAuth2Error, Claims } from './context';
 import { createMemoryStorage } from './memory-storage';
+import { SwedishBankIDTransaction, Transaction } from './transaction';
+import useAppState from './hooks/useAppState';
 
 if (typeof global.TextEncoder === "undefined") {
   global.TextEncoder = TextEncoding.TextEncoder;
@@ -29,6 +32,15 @@ interface CriiptoVerifyProviderOptions {
   children: React.ReactNode
 }
 
+interface SwedishBankIDInitial {
+  cancelUrl: string
+  completeUrl: string
+  launchLinks: {
+    customFileHandlerUrl: string
+    universalLink: string
+  }
+}
+
 function generatePKCE() {
   return generatePlatformPKCE({
     getRandomValues: crypto.getRandomValues.bind(crypto),
@@ -45,6 +57,21 @@ const CriiptoVerifyProvider = (props: CriiptoVerifyProviderOptions) : JSX.Elemen
 
   const [error, setError] = useState<OAuth2Error | Error | null>(null);
   const [claims, setClaims] = useState<Claims | null>(null);
+  const [transaction, setTransaction] = useState<Transaction | null>(null);
+
+  useAppState(() => {
+    if (!transaction) return;
+    transaction.onForeground();
+  }, [transaction]);
+
+  useEffect(() => {
+    if (!transaction) return;
+    const urlCallback : Linking.URLListener = (event) => {
+      transaction.onUrl(event.url);
+    }
+    const subscription = Linking.addEventListener('url', urlCallback);
+    return () => subscription.remove();
+  }, [transaction]);
 
   const login : CriiptoVerifyContextInterface["login"] = useCallback(async (acrValues, redirectUri) => {
     const discovery = await openIDConfigurationManager.fetch();
@@ -52,17 +79,16 @@ const CriiptoVerifyProvider = (props: CriiptoVerifyProviderOptions) : JSX.Elemen
     const authorizeUrl = buildAuthorizeURL(discovery, {
       redirect_uri: redirectUri,
       scope: 'openid',
-      response_mode: 'query',
+      response_mode: acrValues === 'urn:grn:authn:se:bankid:same-device' ? 'json' : 'query',
       response_type: 'code',
       acr_values: acrValues,
       code_challenge: pkce.code_challenge,
       code_challenge_method: pkce.code_challenge_method,
-      prompt: 'login'
+      prompt: 'login',
+      login_hint: `appswitch:${Platform.OS}`
     });
 
-    const result = await WebBrowser.openAuthSessionAsync(authorizeUrl.href, redirectUri);
-    if (result.type === 'success') {
-      const url = new URL(result.url);
+    async function handleURL(url: URL) {
       if (url.searchParams.get('code')) {
         const response = await codeExchange(discovery, {
           code: url.searchParams.get('code')!,
@@ -73,10 +99,9 @@ const CriiptoVerifyProvider = (props: CriiptoVerifyProviderOptions) : JSX.Elemen
         if ("error" in response) {
           throw new OAuth2Error(response.error, response.error_description, response.state);
         } else if ("id_token" in response) {
-          console.log(response.id_token);
           return {
             id_token: response.id_token,
-            claims: jwtDecode(response.id_token)
+            claims: jwtDecode<Claims>(response.id_token)
           }
         } else {
           throw new Error('Unexpected code exchange response: ' + JSON.stringify(response));
@@ -90,10 +115,37 @@ const CriiptoVerifyProvider = (props: CriiptoVerifyProviderOptions) : JSX.Elemen
 
         throw error;
       } else {
-        throw new Error('Unexpected URL response: ' + result.url);
+        throw new Error('Unexpected URL response: ' + url.href);
       }
-    } else if (result.type === 'cancel' || result.type === 'dismiss') {
-      throw new OAuth2Error('access_denied', 'User cancelled/dismissed browser');
+    }
+
+    if (acrValues === 'urn:grn:authn:se:bankid:same-device') {
+      const authorizeResponse = await fetch(authorizeUrl);
+      if (authorizeResponse.status >= 400) throw new Error(await authorizeResponse.text());
+      const authorizePayload : SwedishBankIDInitial = await authorizeResponse.json();
+      
+      const transactionPromise = new Promise<void>((resolve, reject) => {
+        const transaction = new SwedishBankIDTransaction(redirectUri, resolve, async () => {
+          await fetch(authorizePayload.cancelUrl);
+          reject(new OAuth2Error('access_denied', 'User cancelled login'));
+        });
+
+        setTransaction(transaction);
+      });
+
+      await Linking.openURL(authorizePayload.launchLinks.universalLink);
+      await transactionPromise;
+      
+      const completeResponse = await fetch(authorizePayload.completeUrl);
+      if (completeResponse.status >= 400) throw new Error(await completeResponse.text());
+      const completePayload : {location: string} = await completeResponse.json();
+      return await handleURL(new URL(completePayload.location));
+    }
+
+    const result = await WebBrowser.openAuthSessionAsync(authorizeUrl.href, redirectUri);
+    if (result.type === 'success') {
+      const url = new URL(result.url);
+      return await handleURL(url);
     } else {
       throw new Error('Unexpected browser results: ' + JSON.stringify(result));
     }
