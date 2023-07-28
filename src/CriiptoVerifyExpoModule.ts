5043474477e0806012cfea2418bbaa75a62fe90a
nmoskaleva <natalia.moskaleva@criipto.com>
2023-07-28 14:18:32 +0200

    fix for required module

--- src/CriiptoVerifyExpoModule.ts
+++ src/CriiptoVerifyExpoModule.ts
@@ -1,10 +1,12 @@
 import { requireNativeModule } from 'expo-modules-core';
 
-// It loads the native module object from the JSI or falls back to
-// the bridge module (from NativeModulesProxy) if the remote debugger is on.
-const module = requireNativeModule('CriiptoVerifyExpo');
-
-export async function start(params: {authorizeUrl: string, redirectUri: string}) : Promise<string | null> {
+export async function start(params: {
+  authorizeUrl: string;
+  redirectUri: string;
+}): Promise<string | null> {
+  // It loads the native module object from the JSI or falls back to
+  // the bridge module (from NativeModulesProxy) if the remote debugger is on.
+  const module = requireNativeModule('CriiptoVerifyExpo');
   const response = await module.startAsync(params);
   return response;
 }
